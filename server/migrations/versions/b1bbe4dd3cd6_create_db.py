6b7eeae6ec4e648e9c6b3d8473548bb8ad9259ee
Jerry Janvier <jerrythefilmmaker@gmail.com>
2024-04-11 22:51:20 -0400

    frontend, backend templates

---
+++ server/migrations/versions/b1bbe4dd3cd6_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: b1bbe4dd3cd6
+Revises: 
+Create Date: 2024-04-11 22:17:59.930872
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b1bbe4dd3cd6'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
