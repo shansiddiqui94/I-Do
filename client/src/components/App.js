b42bd13d661ff31d06a8d57bec10ebcce869be98
David <Marin9737@gmail.com>
2024-04-16 10:22:33 -0400

    react frontend start

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
-
+import React from "react";
 function App() {
   return <h1>Project Client</h1>;
 }
---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "React";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <p>I-Do</p>
+    </div>
+  );
+}
+
+export default App;
